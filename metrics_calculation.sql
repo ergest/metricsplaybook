8ac7321f4a2f9a29488e59d7184b2457d1493152
Ergest Xheblati <ergest@gmail.com>
2022-10-09 15:18:03 -0400

    added new metric

--- metrics_calculation.sql
+++ metrics_calculation.sql
@@ -1,12 +1,48 @@
+/**
+ * Metric: NewRR
+ */
+with contract_paid as (
+    select
+        customer_id,
+        timestamp,
+        activity,
+        revenue_impact,
+        dc.segment
+    from
+        contract_stream cs
+        join dim_customer dc on cs.customer_id = dc.id 
+    where
+        activity = 'contract_paid'
+        and activity_occurrence = 1
+)
 select
-    customer_id,
-    "timestamp",
-    activity,
-    revenue_impact,
-    dc.segment
+    date_trunc('month', timestamp) as month,
+    sum(revenue_impact) as revenue
 from
-    contract_stream cs
-    join dim_customer dc on cs.customer_id = dc.id 
-where
-    activity = 'contract_paid'
-    and activity_occurrence = 1;
+    contract_paid
+group by 1
+order by 1;
+
+/**
+ * Metric: ExpansionRR
+ */
+with contract_upgraded as (
+    select
+        customer_id,
+        timestamp,
+        activity,
+        revenue_impact,
+        dc.segment
+    from
+        contract_stream cs
+        join dim_customer dc on cs.customer_id = dc.id 
+    where
+        activity = 'contract_upgraded'
+)
+select
+    date_trunc('month', timestamp) as month,
+    sum(revenue_impact) as revenue
+from
+    contract_paid
+group by 1
+order by 1;
