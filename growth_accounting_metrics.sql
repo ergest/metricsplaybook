95867f8de34444b14b09513ff3e39ed2fb77b350
Ergest Xheblati <ergest@gmail.com>
2022-10-14 19:13:31 -0400

    WIIP

--- growth_accounting_metrics.sql
+++ growth_accounting_metrics.sql
@@ -1,7 +1,19 @@
 /**
  * Metric: NewRR
  */
-with contract_paid as (
+with customer_cohort as (
+    select
+        customer_id,
+        timestamp as first_signed_date,
+        date_trunc('month', timestamp) as first_signed_month,
+        date_trunc('week', timestamp) as first_signed_week
+    from
+        contract_stream 
+    where 
+        activity = 'contract_signed'
+        and activity_occurrence = 1
+),
+contract_paid as (
     select
         customer_id,
         timestamp,
@@ -10,11 +22,14 @@ with contract_paid as (
         dc.segment
     from
         contract_stream cs
-        join dim_customer dc on cs.customer_id = dc.id 
+        join dim_customer dc on cs.customer_id = dc.id
+        join customer_cohort cc on cs.customer_id = cs.customer_id
     where
         activity = 'contract_paid'
         and activity_occurrence = 1
-)
+);
+--sample aggregation
+/*
 select
     date_trunc('month', timestamp) as month,
     sum(revenue_impact) as revenue
@@ -22,6 +37,7 @@ from
     contract_paid
 group by 1
 order by 1;
+*/
 
 /**
  * Metric: ExpansionRR
@@ -43,6 +59,10 @@ select
     date_trunc('month', timestamp) as month,
     sum(revenue_impact) as revenue
 from
-    contract_paid
+    contract_upgraded
 group by 1
 order by 1;
+
+/*
+ * renewed = retaiend
+ */
