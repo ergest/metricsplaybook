053fc64b5d3f71ec38d25d8292383b7f12ce71e9
Ergest Xheblati <ergest@gmail.com>
2022-10-26 22:46:43 -0400

    updted logic

--- models/growth_accounting/committed_revenue.sql
+++ models/growth_accounting/committed_revenue.sql
@@ -2,32 +2,10 @@
     config(materialized = 'view')
 }}
 
-with total_rr as (
-    select
-        coalesce(date_trunc('month', nr.timestamp), date_trunc('month', er.timestamp), date_trunc('month', cr.timestamp), date_trunc('month', cxr.timestamp)) as month,
-        sum(coalesce(nr.revenue_impact,0) + coalesce(er.revenue_impact,0) - coalesce(cr.revenue_impact,0) - coalesce(cxr.revenue_impact,0)) as net_recurring_revenue
-    from
-        {{ ref('new_revenue') }} nr
-        full outer join {{ ref('expansion_revenue') }} er
-            on nr.customer_id = er.customer_id
-            and date_trunc('month', nr.timestamp) = date_trunc('month', er.timestamp)
-        full outer join {{ ref('contraction_revenue') }} cr
-            on nr.customer_id = cr.customer_id
-            and date_trunc('month', nr.timestamp) = date_trunc('month', cr.timestamp)
-        full outer join {{ ref('churned_revenue') }} cxr
-            on nr.customer_id = cxr.customer_id
-            and date_trunc('month', nr.timestamp) = date_trunc('month', cxr.timestamp)
-    group by 1
-),
-next_period_net_rr as (
-    select
-        month,
-        sum(net_recurring_revenue) over(partition by month order by month) as total_rr
-    from
-        net_revenue    
-)
 select
-    month,
-    sum(net_recurring_revenue) over(partition by month order by month) as total_rr
+    tr."month" as this_period,
+    nr."month" + interval 1 month as next_period,
+    tr.total_rr + nr.net_recurring_revenue 
 from
-    net_revenue
+    {{ ref('total_revenue') }} tr
+    join {{ ref('net_revenue') }} nr on tr."month" = nr."month" + interval 1 month
