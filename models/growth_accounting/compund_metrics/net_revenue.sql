b6b82e41f183a4f52c6a8e20e5c367bacc77a957
Ergest Xheblati <ergest@gmail.com>
2023-01-08 16:31:12 -0500

    fixed join

--- models/growth_accounting/compund_metrics/net_revenue.sql
+++ models/growth_accounting/compund_metrics/net_revenue.sql
@@ -3,17 +3,14 @@
 -}}
 
 select
-    nr.series_month,
+    nr.series_month as month,
     sum(coalesce(nr.revenue_impact,0) + coalesce(er.revenue_impact,0) - coalesce(cr.revenue_impact,0) - coalesce(ch.revenue_impact,0)) as net_recurring_revenue
 from
     {{ ref('new_mrr') }} nr
     full outer join {{ ref('expansion_mrr') }} er
         on nr.series_month = er.series_month
-        and nr.customer_id = er.customer_id
     full outer join {{ ref('contraction_mrr') }} cr
         on nr.series_month = cr.series_month
-        and nr.customer_id = cr.customer_id
     full outer join {{ ref('churned_mrr') }} ch
         on nr.series_month = ch.series_month
-        and nr.customer_id = ch.customer_id
 group by 1
