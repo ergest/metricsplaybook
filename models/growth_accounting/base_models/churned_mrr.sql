379156b21f76680bd6a1c299f89d87ac59e646e8
Ergest Xheblati <ergest@gmail.com>
2023-01-08 12:37:05 -0500

    made all models into OBTs

--- models/growth_accounting/base_models/churned_mrr.sql
+++ models/growth_accounting/base_models/churned_mrr.sql
@@ -1,13 +1,20 @@
-{{
+{{-
     config(materialized = 'view')
-}}
+-}}
 
 select
-    customer_id,
-    timestamp,
-    activity,
-    revenue_impact
+    c.segment,
+    c.channel,
+    c.cohort,
+    m.timestamp,
+    m.revenue_impact,
+    m.activity,
+    dt.*
 from
-    {{ ref('contract_stream') }}
+    {{ ref('contract_stream') }} m
+    join {{ ref('dim_customer')}} c
+        on m.customer_id = c.id
+    join {{ ref('dim_date') }} dt
+        on dt.series_day = date_trunc('day', m.timestamp)
 where
-    activity = 'customer_churn_committed'
+    m.activity = 'customer_churn_committed'
