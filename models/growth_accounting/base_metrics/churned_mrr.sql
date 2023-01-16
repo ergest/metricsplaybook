ae44e74b06b84ea2370e46b8040ae502e3d97a30
Ergest Xheblati <ergest@gmail.com>
2023-01-16 14:57:53 -0500

    removed date dim join

--- models/growth_accounting/base_metrics/churned_mrr.sql
+++ models/growth_accounting/base_metrics/churned_mrr.sql
@@ -9,13 +9,10 @@ select
     c.cohort,
     m.timestamp,
     m.revenue_impact,
-    m.activity,
-    dt.*
+    m.activity
 from
     {{ ref('contract_stream') }} m
     join {{ ref('dim_customer')}} c
         on m.customer_id = c.id
-    join {{ ref('dim_date') }} dt
-        on dt.series_day = date_trunc('day', m.timestamp)
 where
     m.activity = 'customer_churn_committed'
