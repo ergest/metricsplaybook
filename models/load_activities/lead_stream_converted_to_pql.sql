ba22149dd02167bdbda1b2ecdf5bea4c966565e9
Ergest Xheblati <ergest@gmail.com>
2023-03-16 23:00:04 -0400

    updated all models

--- models/load_activities/lead_stream_converted_to_pql.sql
+++ models/load_activities/lead_stream_converted_to_pql.sql
@@ -1,5 +1,15 @@
-{{
-  config(materialized = 'table')
-}}
-select *
-from read_csv_auto('raw_csvs/lead_stream_converted_to_pql.csv')
+{{-
+    config(materialized = 'table')
+-}}
+
+{{-
+generate_fake_data (
+    activity_name = 'converted_to_pql',
+    has_revenue_impact = false,
+    feature_json_dict = '{"subcsription_type": ["mrr", "arr"],
+                         "segment": ["seg1", "seg2", "seg3", "seg4"],
+                         "plan": ["plan1", "plan2", "plan3"],
+                         "csm": ["csm1", "csm2", "csm3", "csm4", "csm5"],
+                         "mrr_tier": ["tier1", "tier2"]}'
+  )
+-}}
